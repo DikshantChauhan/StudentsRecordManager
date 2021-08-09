877a1f08a5e46106ff485070bdf5bd7ae5885a5b
DikshantChauhan <chauhandikshant2734982@gmail.com>
2021-08-09 21:36:54 +0530

    fetch group by id created

--- src/Components/Api/Groups.ts
+++ src/Components/Api/Groups.ts
@@ -8,13 +8,22 @@ interface GroupRequest {
     status: "all-groups"
 }
 
-interface GroupResponse {
+/* interface GroupResponse {
     data: Group[]
-}
+} */
 
 export const fetchGroups = (data: GroupRequest) =>{
     return axios.get("https://api-dev.domecompass.com/groups", 
     { params: data })
+    .then((response) =>{
+        return response.data.data
+    }).catch((error) =>{
+        console.error(error)
+    })
+}
+
+export const fetchGroup = (id: number) =>{
+    return axios.get(`https://api-dev.domecompass.com/groups/${id}`)
     .then((response) =>{
         return response.data.data
     }).catch((error) =>{
