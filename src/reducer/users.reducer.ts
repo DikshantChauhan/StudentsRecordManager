6c3ff3cf76b63f8409c93a239b8b656f8cc25a75
DikshantChauhan <chauhandikshant2734982@gmail.com>
2021-08-09 22:27:43 +0530

    some code replaced with resuable normalizer function

--- src/reducer/users.reducer.ts
+++ src/reducer/users.reducer.ts
@@ -1,27 +1,24 @@
 import { AnyAction, Reducer } from "redux";
+import { EntityState } from "../Components/Models/Entity";
 import { User } from "../Components/Models/User";
 import { actionKey } from "../store";
+import { normalizeOne } from "./helperFunctions";
 
-export interface UsersState {
-    byId: {
-        [id: number]: User
-    }
+export interface UsersState extends EntityState<User>{
 }
 
 const initialValue: UsersState = {
-    byId: {}
+    byIds: {}
 }
 
 export const usersReducer: Reducer<UsersState> = 
     (currentState = initialValue, dispatchedAction: AnyAction) =>{
         switch(dispatchedAction.type){
             case actionKey.ME_LOGIN:
+                const newState = 
+                    normalizeOne(currentState, dispatchedAction.payload) as UsersState
                 return {
-                    ...currentState, 
-                    byId: {
-                        ...currentState.byId, 
-                        [dispatchedAction.payload.id]: dispatchedAction.payload
-                        }
+                    ...newState
                     }
 
             default:
