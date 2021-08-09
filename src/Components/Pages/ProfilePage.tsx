0e5bc9ecfdd3bf7c90512943a7c3ba6f2ddbb053
DikshantChauhan <chauhandikshant2734982@gmail.com>
2021-08-09 22:18:25 +0530

    onSubmit meUpdate added

--- src/Components/Pages/ProfilePage.tsx
+++ src/Components/Pages/ProfilePage.tsx
@@ -5,6 +5,7 @@ import ButtonSolid from "../Button/ButtonSolid";
 import InputLabeled from "../Input/InputLabeled";
 import { useAppSelector } from "../../store";
 import { meSelector } from "../../selector/auth.selector";
+import { meUpdate } from "../Api/Auth";
 
 interface Props{}
 
@@ -13,26 +14,28 @@ const ProfilePage: React.FC<Props> = () => {
 
     const formik = useFormik({
         initialValues: {
-            "First Name": user?.first_name,
-            "Last Name": user?.last_name,
-            "Phone Number": user?.phone_number,
+            "first_name": user?.first_name,
+            "last_name": user?.last_name,
+            "phone_number": user?.phone_number,
+            /* "profile_pic_url": user?.profile_pic_url, */
         },
         validationSchema: yup.object().shape({
-            "First Name": yup.string().required().min(2),
-            "Last Name": yup.string().required(),
-            "Phone Number": yup.string().required().min(10),
+            "first_name": yup.string().required().min(2),
+            "last_name": yup.string().required(),
+            "phone_number": yup.string().required().min(10),
+            /* "profile_pic_url": yup.string().required(), */
         }),
         onSubmit: (userData) =>{
-            console.log(userData)
+            meUpdate(userData);
         }
     })
 
     return(
-        <div className={`bg-white mx-2 p-2 mt-6 rounded-lg shadow-sm relative`}>
+        <div className={`bg-white mx-2 p-2 rounded-lg shadow-sm relative`}>
             <h5 className={`text-lg mt-1 mx-2 mb-16 font-medium text-gray-600`}>GENERAL INFORMATION</h5>
             <form onSubmit={formik.handleSubmit} className={`m-5`}>
                 <div className={`mx-4 mb-4 pt-36 relative`}>
-                    <img className={`w-36 h-36 absolute top-0 left-0 rounded-lg`} src={user?.profile_pic_url!} alt="Avatar" />
+                    <img className={`w-36 h-36 absolute top-0 left-0 rounded-lg`} src={user?.profile_pic_url} alt="Avatar" />
                     <p className={`text-primary-main my-2`}>Upload Picture</p>
                 </div>
                 <div className={`flex`}>
@@ -40,9 +43,9 @@ const ProfilePage: React.FC<Props> = () => {
                         type="text"
                         placeholder="First Name"
                         required
-                        {...formik.getFieldProps("First Name")}
-                        touched={formik.touched["First Name"]}
-                        error={formik.errors["First Name"]}
+                        {...formik.getFieldProps("first_name")}
+                        touched={formik.touched["first_name"]}
+                        error={formik.errors["first_name"]}
                         label="First Name"
                         className={`w-1/2 mx-4 mb-5`}
                     />
@@ -50,20 +53,30 @@ const ProfilePage: React.FC<Props> = () => {
                         type="text"
                         placeholder="Last Name"
                         required
-                        {...formik.getFieldProps("Last Name")}
-                        touched={formik.touched["Last Name"]}
-                        error={formik.errors["Last Name"]}
+                        {...formik.getFieldProps("last_name")}
+                        touched={formik.touched["last_name"]}
+                        error={formik.errors["last_name"]}
                         label="Last Name"
                         className={`w-1/2 mx-4 mb-5`}
                     />
-                </div>
+                </div>{/* 
+                <InputLabeled 
+                    type="text"
+                    placeholder="profile_pic_url"
+                    required
+                    {...formik.getFieldProps("profile_pic_url")}
+                    touched={formik.touched["profile_pic_url"]}
+                    error={formik.errors["profile_pic_url"]}
+                    label="profile_pic_url"
+                    className={`mx-4 mb-5`}
+                /> */}
                 <InputLabeled 
                     type="text"
                     placeholder="Phone Number"
                     required
-                    {...formik.getFieldProps("Phone Number")}
-                    touched={formik.touched["Phone Number"]}
-                    error={formik.errors["Phone Number"]}
+                    {...formik.getFieldProps("phone_number")}
+                    touched={formik.touched["phone_number"]}
+                    error={formik.errors["phone_number"]}
                     label="Phone Number"
                     className={`mx-4 mb-5`}
                 />
