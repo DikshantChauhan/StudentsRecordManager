2adde81994c03a79803c861d919808d42e59beef
DikshantChauhan <chauhandikshant2734982@gmail.com>
2021-08-09 22:43:00 +0530

    normalizer function shift to new helperFunction file

---
+++ src/reducer/helperFunctions.ts
@@ -0,0 +1,17 @@
+import { Entity, EntityState } from "../Components/Models/Entity"
+
+export const normalizeMany = (state: EntityState, arr: Entity[]) =>{
+    if(arr === []){
+        return state
+    }
+
+    const normalizedArr = arr.reduce((pre, curr) =>{
+        return { ...pre, [curr.id]: curr }
+    }, {})
+
+    return { ...state, byIds: {...state.byIds ,...normalizedArr}}
+}
+
+export const normalizeOne = (state: EntityState, entity: Entity) =>{
+    return { ...state, byIds: {...state.byIds, [entity.id]: entity }}
+}
